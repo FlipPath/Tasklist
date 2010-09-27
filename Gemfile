18944db01ddff1d9fecb23c4c0a5e18a829412b9
Shayne Sweeney <shaynesweeney@me.com>
2010-09-26 21:38:06 -0700

    hi world

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source "http://rubygems.org"
+
+gem "rails", "3.0.0"
+
+gem "mongoid", "2.0.0.beta.18"
+gem "bson_ext", "1.0.4"
+
+# To use debugger
+gem "ruby-debug"
+
+# group :development, :test do
+#   gem "webrat"
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasklist::Application.config.secret_token = '4a1e18c72976befa00c27b6f2a835d5374bb8b741c706fcc7ac90e36a8c8f34550559e2d1130941fff109db9ee47bfff3e3a60ba902ad6f0f7f9c297376bb0a3'
