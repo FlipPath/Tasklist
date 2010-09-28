e3fdde7d0a511565f1b9b9b3ce63ceb199dd6d24
tylersmalley <github@tylersmalley.com>
2010-09-28 01:40:10 -0700

    Created test for list destroy action.

--- test/functional/lists_controller_test.rb
+++ test/functional/lists_controller_test.rb
@@ -2,12 +2,22 @@ require 'test_helper'
 
 class ListsControllerTest < ActionController::TestCase
   context "lists controller" do
+    setup { @list = Factory(:valid_list) }
+    
     context "for create action" do
       setup { post(:create, :format => :js, :list => { :name => "Groceries" }) }
       
       should assign_to(:list).with_kind_of(List)
       
       should render_template(:create)
+    end
+    
+    context "for destroy action" do
+      setup { delete(:destroy, :format => :js, :id => @list.id) }
+      
+      should assign_to(:list).with_kind_of(List)
+      
+      should render_template(:destroy)
     end
   end
 end
