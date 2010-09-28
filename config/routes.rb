1a636197a8ba047db2a2a0f1375ecf306fe4a90d
tylersmalley <github@tylersmalley.com>
2010-09-28 16:11:04 -0700

    Changed to reflect rename of complete to toggle_complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Tasklist::Application.routes.draw do
   resources :lists, :only => [:index, :create, :destroy] do
     resources :tasks, :only => [:create, :destroy] do
       member do
-        put :complete
+        put :toggle_complete
       end
     end
   end
