0ea124a07ffc7ee57a3b5f61e40e89f7d42870ac
Shayne Sweeney <shaynesweeney@me.com>
2010-09-27 23:36:32 -0700

    Updated TaskHelperTest to work as nested resource

--- test/unit/helpers/task_helper_test.rb
+++ test/unit/helpers/task_helper_test.rb
@@ -2,17 +2,19 @@ require 'test_helper'
 
 class TaskHelperTest < ActionView::TestCase
   context "for task helper" do
+    setup { @list = Factory(:list_with_item) }
+    setup { @first_task = @list.tasks.first }
     
     context "for link_class method" do
-      setup { @task = Factory(:valid_task, :completed => true) }
+      setup { @first_task.completed = true }
       
       should "return class completed" do
-        assert_equal "completed", link_class(@task)
+        assert_equal "completed", link_class(@first_task)
       end
       
       should "return nil" do
-        @task.completed = false
-        assert_nil link_class(@task)
+        @first_task.completed = false
+        assert_nil link_class(@first_task)
       end
     end
   end
