85a627d4d1b6952fb11a1e75fe7049de85338f33
tylersmalley <github@tylersmalley.com>
2010-09-28 01:36:19 -0700

    Added destroy method to ListController.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,4 +7,12 @@ class ListsController < ApplicationController
     end
   end
   
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    
+    respond_to do |format|
+      format.js
+    end
+  end
 end
