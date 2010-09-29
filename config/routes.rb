9a277fc890ff30c234ab191b62d37733ff79763c
Shayne Sweeney <shaynesweeney@me.com>
2010-09-28 22:20:23 -0700

    Updated root to home#index and added users and devise routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Tasklist::Application.routes.draw do
+  root :to => 'home#index'
+  
+  devise_for :users
+  resources :users, :only => :show
+  namespace :user do
+    root :to => redirect('/lists')
+  end
+
   resources :lists, :only => [:index, :create, :destroy] do
     resources :tasks, :only => [:create, :destroy] do
       member do
@@ -6,6 +14,4 @@ Tasklist::Application.routes.draw do
       end
     end
   end
-  
-  root :to => 'lists#index'
 end
