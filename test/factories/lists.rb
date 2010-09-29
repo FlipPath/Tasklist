c07fa23973ec70791d6b8f9e873d7bb45f2b417d
tylersmalley <github@tylersmalley.com>
2010-09-29 06:49:58 -0700

    Created factory for list_with_ten_items.

--- test/factories/lists.rb
+++ test/factories/lists.rb
@@ -12,6 +12,13 @@ end
 Factory.define :list_with_item, :class => List do |f|
   f.name "At the office"
   f.after_build do |l|
-    l.tasks << l.tasks.create(:task => "Check email")
+    l.tasks.create(:task => "Check email")
+  end
+end
+
+Factory.define :list_with_ten_items, :class => List do |f|
+  f.name "Lipsum"
+  f.after_build do |l|
+    Faker::Lorem.sentences(10).each { |s| l.tasks.create(:task => s) }
   end
 end
