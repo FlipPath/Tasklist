6709576d1b072dfa1584b936b9886f02f8d2df40
Shayne Sweeney <shaynesweeney@me.com>
2010-09-28 22:22:22 -0700

    Updated ListsControllerTest to test index action and include authorization

--- test/functional/lists_controller_test.rb
+++ test/functional/lists_controller_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class ListsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+  
   context "lists controller" do
-    setup { @list = Factory(:valid_list) }
+    setup do
+      User.destroy_all
+      @user = Factory(:user)
+      sign_in @user
+      @list = Factory(:valid_list)
+      @user.lists << @list
+    end
+    
+    context "for index action" do
+      setup { get(:index) }
+      should assign_to(:lists)
+      should respond_with(:success)
+      should render_template(:index)
+    end
     
     context "for create action" do
       setup { post(:create, :format => :js, :list => { :name => "Groceries" }) }
