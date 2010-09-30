27f6dc3f2cc11400d3d329dc84cd7b35befb6882
Shayne Sweeney <shaynesweeney@me.com>
2010-09-29 19:21:46 -0700

    Added route for /pusher/auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Tasklist::Application.routes.draw do
   root :to => 'lists#index', :constraints => lambda {|r| r.env["warden"].authenticate? }
   root :to => 'home#index'
   
+  get "/lists" => 'lists#index', :as => "user_root"
+  post "/pusher/auth" => 'pusher#auth', :format => :js
+  
   devise_for :users
   resources :users, :only => :show do
     collection do
       get :search
     end
   end
-  
-  get "/lists" => 'lists#index', :as => "user_root"
   
   resources :lists, :only => [:index, :create, :destroy] do
     member do
