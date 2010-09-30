8446ca2e9aecea30bb4405b13a84dd76b076e043
Shayne Sweeney <shaynesweeney@me.com>
2010-09-30 02:08:22 -0700

    Implemented web socket notification on task changes and moved rendering inline

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,28 +1,47 @@
 class TasksController < ApplicationController
   before_filter :load_list
+  before_filter :load_channel, :only => [:create, :toggle_complete, :destroy]
   before_filter :load_task, :only => [:toggle_complete, :destroy]
   
   def create
     @task = @list.tasks.create(:task => params[:task][:task])
     
+    data = {
+      :type => "tasks:create", :list_id => "#{@list.id}",
+      :task_html => render_haml("task", :task => @task)
+    };
+    
+    @channel.trigger("task-create", data, params[:socket_id])
+    
     respond_to do |format|
-      format.js
+      format.js { jq_trigger data }
     end
   end
   
   def toggle_complete
     @task.toggle_complete
     
+    data = {
+      :type => "tasks:complete", :task_id => "#{@task.id}",
+      :task_html => render_haml("task", :task => @task)
+    }
+    
+    @channel.trigger("task-toggle-complete", data);
+    
     respond_to do |format|
-      format.js
+      format.js { jq_trigger data }
     end
   end
   
   def destroy
     @task.destroy
     
+    data = { :type => "tasks:destroy", :task_id => "#{@task.id}" }
+    
+    @channel.trigger("task-destroy", data);
+    
     respond_to do |format|
-      format.js
+      format.js { jq_trigger data }
     end
   end
   
@@ -34,5 +53,9 @@ class TasksController < ApplicationController
   
   def load_task
     @task = @list.tasks.find(params[:id])
+  end
+  
+  def load_channel
+    @channel = Pusher["private-list-#{@list.id}"]
   end
 end
