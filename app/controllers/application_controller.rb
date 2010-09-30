c2045be64a10e7525a4a8ead2bfa9f972a7f9604
Shayne Sweeney <shaynesweeney@me.com>
2010-09-30 02:08:04 -0700

    Added jq_trigger and render_haml helper methods for cleaner markup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def jq_trigger(data)
+    render :js => "$(document).trigger(#{data.to_json});"
+  end
+  
+  def render_haml(partial, locals={})
+    render_to_string :partial => "#{partial}.html.haml", :locals => locals
+  end
 end
