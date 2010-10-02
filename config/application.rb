0cae85ea52d6b257e32e11503c34b58a6684d11f
Shayne Sweeney <shaynesweeney@me.com>
2010-10-01 19:39:39 -0700

    Include prototype helper extension in application.rb

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
+require "lib/extensions/prototype_ext"
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
