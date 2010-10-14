60ad5eb3aa599875a2efb7a31405859c18959af5
tylersmalley <github@tylersmalley.com>
2010-10-14 02:00:32 -0700

    Changed layout to use application when logged in and home when logged out.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Tasklist::Application.routes.draw do
-  root :to => 'lists#index', :constraints => lambda {|r| r.env["warden"].authenticate? }
+  root :to => 'groups#index', :constraints => lambda {|r| r.env["warden"].authenticate? }
   root :to => 'home#index'
   
-  get "/" => 'lists#index', :as => "user_root"
+  get "/" => 'groups#index', :as => "user_root"
   post "/pusher/auth" => 'pusher#auth', :format => :js
   
   devise_for :users
